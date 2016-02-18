bfcf1e68b5a9dc424ede7e8adf5feb954a728743
xuxx <xuxx09@126.com>
2016-02-18 22:32:47 +0800

    the basic class of the linear list, this class is a ADT

---
+++ linearlist/LinearList.h
@@ -0,0 +1,25 @@
+/**
+ *头文件定义了线性表的抽象基类
+ */
+enum bool{false, true};
+template <class T>
+class LinearList
+{
+public:
+    LinearList();
+    ~LinearList();
+    virtual int Size()const = 0;
+    virtual int Length()const = 0;
+    virtual int Search(T& x)const = 0;
+    virtual int Locate(int i)const = 0;
+    virtual bool getDate(int i, T& x)const = 0;
+    virtual void setDate(int i, T& x)const = 0;
+    virtual bool Insert(int i, T& x) = 0;
+    virtual bool Remove(int i, T& x) = 0;
+    virtual bool IsEmpty()const = 0;
+    virtual bool IsFull()const = 0;
+    virtual void Sort() = 0;
+    virtual void input() = 0;
+    virtual void output() = 0;
+    virtual LinearList<T> operator=(LinearList<T>& L) = 0;
+
